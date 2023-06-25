cc019cec1d8087a711b88f1d6a40ec1320a9fb77
nishaj0 <njnishaj0@gmail.com>
2023-06-25 12:30:45 +0530

    position fixed of header, added container classes

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -14,82 +14,84 @@ import "./footer.css";
 
 function Footer() {
    return (
-      <div className="wallHub__footer section__padding">
-         <div className="wallHub__footer-links">
-            <div className="wallHub__footer-links_logo">
-               <img src={logoImg} alt="logo image" />
+      <div className="footer__container">
+         <div className="wallHub__footer container section__padding">
+            <div className="wallHub__footer-links">
+               <div className="wallHub__footer-links_logo">
+                  <img src={logoImg} alt="logo image" />
+                  <p>
+                     Discover and download our AI-generated wallpapers for a
+                     visually captivating experience.
+                  </p>
+               </div>
+               <div className="wallHub__footer-link">
+                  <h4>Company</h4>
+                  <ul>
+                     <li>
+                        <a className="" href="">
+                           About Us
+                        </a>
+                     </li>
+                     <li>
+                        <a href="">Contact Us</a>
+                     </li>
+                     <li>
+                        <a href="">Terms and Conditions</a>
+                     </li>
+                     <li>
+                        <a href="">Privacy Policy</a>
+                     </li>
+                  </ul>
+               </div>
+               <div className="wallHub__footer-link">
+                  <h4>Helpful Links</h4>
+                  <ul>
+                     <li>
+                        <a href="">FAQs</a>
+                     </li>
+                     <li>
+                        <a href="">Help Center</a>
+                     </li>
+                  </ul>
+               </div>
+               <div className="wallHub__footer-link">
+                  <h4>Connect with Us</h4>
+                  <a href="">
+                     <RiInstagramFill
+                        color="#000"
+                        size={27}
+                        className="wallHub__footer-link_social"
+                     />
+                  </a>
+                  <a href="">
+                     <RiDiscordFill
+                        color="#000"
+                        size={27}
+                        className="wallHub__footer-link_social"
+                     />
+                  </a>
+                  <a href="">
+                     <RiTwitterFill
+                        color="#000"
+                        size={27}
+                        className="wallHub__footer-link_social"
+                     />
+                  </a>
+                  <a href="">
+                     <RiGithubFill
+                        color="#000"
+                        size={27}
+                        className="wallHub__footer-link_social"
+                     />
+                  </a>
+               </div>
+            </div>
+            <div className="wallHub__footer-copyright">
                <p>
-                  Discover and download our AI-generated wallpapers for a
-                  visually captivating experience.
+                  © <a href="https://github.com/nishaj0">nishaj0</a> All right
+                  reserved
                </p>
             </div>
-            <div className="wallHub__footer-link">
-               <h4>Company</h4>
-               <ul>
-                  <li>
-                     <a className="" href="">
-                        About Us
-                     </a>
-                  </li>
-                  <li>
-                     <a href="">Contact Us</a>
-                  </li>
-                  <li>
-                     <a href="">Terms and Conditions</a>
-                  </li>
-                  <li>
-                     <a href="">Privacy Policy</a>
-                  </li>
-               </ul>
-            </div>
-            <div className="wallHub__footer-link">
-               <h4>Helpful Links</h4>
-               <ul>
-                  <li>
-                     <a href="">FAQs</a>
-                  </li>
-                  <li>
-                     <a href="">Help Center</a>
-                  </li>
-               </ul>
-            </div>
-            <div className="wallHub__footer-link">
-               <h4>Connect with Us</h4>
-               <a href="">
-                  <RiInstagramFill
-                     color="#000"
-                     size={27}
-                     className="wallHub__footer-link_social"
-                  />
-               </a>
-               <a href="">
-                  <RiDiscordFill
-                     color="#000"
-                     size={27}
-                     className="wallHub__footer-link_social"
-                  />
-               </a>
-               <a href="">
-                  <RiTwitterFill
-                     color="#000"
-                     size={27}
-                     className="wallHub__footer-link_social"
-                  />
-               </a>
-               <a href="">
-                  <RiGithubFill
-                     color="#000"
-                     size={27}
-                     className="wallHub__footer-link_social"
-                  />
-               </a>
-            </div>
-         </div>
-         <div className="wallHub__footer-copyright">
-            <p>
-               © <a href="https://github.com/nishaj0">nishaj0</a> All right
-               reserved
-            </p>
          </div>
       </div>
    );
--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -12,118 +12,120 @@ function Header() {
    const menuLinkClass = "wallHub__header-menu_links-link";
 
    return (
-      <div className="wallHub__header">
-         <div className="wallHub__header-main">
-            <Link to="/" className="wallHub__header__logo">
-               <img src={logoImg} alt="" />
-            </Link>
-
-            <nav className="wallHub__nav">
-               <div className="wallHub__nav-links">
-                  <NavLink
-                     to="/"
-                     className={({ isActive }) =>
-                        isActive ? activeLinkClass : null
-                     }
-                  >
-                     Home
-                  </NavLink>
-                  <NavLink
-                     to="/catagories"
-                     className={({ isActive }) =>
-                        isActive ? activeLinkClass : null
-                     }
-                  >
-                     Catagories
-                  </NavLink>
-                  <NavLink
-                     to="/contact"
-                     className={({ isActive }) =>
-                        isActive ? activeLinkClass : null
-                     }
-                  >
-                     Contact
-                  </NavLink>
-                  <NavLink
-                     to="/about"
-                     className={({ isActive }) =>
-                        isActive ? activeLinkClass : null
-                     }
-                  >
-                     About
-                  </NavLink>
-               </div>
-               <Link to="/login" className="wallHub__nav-login">
-                  <button>Login</button>
+      <div className="header__container">
+         <div className="wallHub__header container">
+            <div className="wallHub__header-main">
+               <Link to="/" className="wallHub__header__logo">
+                  <img src={logoImg} alt="" />
                </Link>
-               <div className="wallHub__nav-toggler">
-                  {toggleMenu ? (
-                     <RiCloseLine
-                        color="#333333"
-                        size={30}
-                        onClick={() => setToggleMenu(false)}
-                     />
-                  ) : (
-                     <RiMenuLine
-                        color="#333333"
-                        size={27}
-                        onClick={() => setToggleMenu(true)}
-                     />
-                  )}
-               </div>
-            </nav>
-         </div>
-         <div className="wallHub__header-menu_container">
-            {toggleMenu && (
-               <div className="wallHub__header-menu_links">
-                  <NavLink
-                     to="/"
-                     className={({ isActive }) =>
-                        //? here "wallHub__nav-active" class used to give active routes a different color
-                        isActive
-                           ? activeLinkClass + " " + menuLinkClass
-                           : menuLinkClass
-                     }
-                  >
-                     Home
-                  </NavLink>
-                  <NavLink
-                     to="/catagories"
-                     className={({ isActive }) =>
-                        isActive
-                           ? activeLinkClass + " " + menuLinkClass
-                           : menuLinkClass
-                     }
-                  >
-                     Catagories
-                  </NavLink>
-                  <NavLink
-                     to="/contact"
-                     className={({ isActive }) =>
-                        isActive
-                           ? activeLinkClass + " " + menuLinkClass
-                           : menuLinkClass
-                     }
-                  >
-                     Contact
-                  </NavLink>
-                  <NavLink
-                     to="/about"
-                     className={({ isActive }) =>
-                        isActive
-                           ? activeLinkClass + " " + menuLinkClass
-                           : menuLinkClass
-                     }
-                  >
-                     About
-                  </NavLink>
-                  <div className="wallHub__header-menu_login">
-                     <Link to="/login">
-                        <button>Login</button>
-                     </Link>
+
+               <nav className="wallHub__nav">
+                  <div className="wallHub__nav-links">
+                     <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                           isActive ? activeLinkClass : null
+                        }
+                     >
+                        Home
+                     </NavLink>
+                     <NavLink
+                        to="/catagories"
+                        className={({ isActive }) =>
+                           isActive ? activeLinkClass : null
+                        }
+                     >
+                        Catagories
+                     </NavLink>
+                     <NavLink
+                        to="/contact"
+                        className={({ isActive }) =>
+                           isActive ? activeLinkClass : null
+                        }
+                     >
+                        Contact
+                     </NavLink>
+                     <NavLink
+                        to="/about"
+                        className={({ isActive }) =>
+                           isActive ? activeLinkClass : null
+                        }
+                     >
+                        About
+                     </NavLink>
                   </div>
-               </div>
-            )}
+                  <Link to="/login" className="wallHub__nav-login">
+                     <button>Login</button>
+                  </Link>
+                  <div className="wallHub__nav-toggler">
+                     {toggleMenu ? (
+                        <RiCloseLine
+                           color="#333333"
+                           size={30}
+                           onClick={() => setToggleMenu(false)}
+                        />
+                     ) : (
+                        <RiMenuLine
+                           color="#333333"
+                           size={27}
+                           onClick={() => setToggleMenu(true)}
+                        />
+                     )}
+                  </div>
+               </nav>
+            </div>
+            <div className="wallHub__header-menu_container">
+               {toggleMenu && (
+                  <div className="wallHub__header-menu_links">
+                     <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                           //? here "wallHub__nav-active" class used to give active routes a different color
+                           isActive
+                              ? activeLinkClass + " " + menuLinkClass
+                              : menuLinkClass
+                        }
+                     >
+                        Home
+                     </NavLink>
+                     <NavLink
+                        to="/catagories"
+                        className={({ isActive }) =>
+                           isActive
+                              ? activeLinkClass + " " + menuLinkClass
+                              : menuLinkClass
+                        }
+                     >
+                        Catagories
+                     </NavLink>
+                     <NavLink
+                        to="/contact"
+                        className={({ isActive }) =>
+                           isActive
+                              ? activeLinkClass + " " + menuLinkClass
+                              : menuLinkClass
+                        }
+                     >
+                        Contact
+                     </NavLink>
+                     <NavLink
+                        to="/about"
+                        className={({ isActive }) =>
+                           isActive
+                              ? activeLinkClass + " " + menuLinkClass
+                              : menuLinkClass
+                        }
+                     >
+                        About
+                     </NavLink>
+                     <div className="wallHub__header-menu_login">
+                        <Link to="/login">
+                           <button>Login</button>
+                        </Link>
+                     </div>
+                  </div>
+               )}
+            </div>
          </div>
       </div>
    );
