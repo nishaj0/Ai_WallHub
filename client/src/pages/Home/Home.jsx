8f23200d8f17b90cbf657300666f484b227e9e62
nishaj0 <njnishaj0@gmail.com>
2023-06-27 12:37:11 +0530

    added jsx for home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import './home.css'
+import React from "react";
+import BackgroundImg from "../../assets/images/landscape_mountain_view_1536 x512px.webp";
+import { RiSearchLine } from "react-icons/ri";
+
+import "./home.css";
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+   return (
+      <div className="wallHub__home section__padding">
+         <div className="wallHub__home-search_container ">
+            <h1>Discover the Future of Wallpapers</h1>
+            <h2> Embrace the Beauty of AI-Generated Wallpapers</h2>
+            <div className="wallHub__home-search_bar">
+               <form action="get">
+                  <input
+                     type="text"
+                     placeholder="Search AI-Generated Wallpapers"
+                  />
+                  <button type="submit">
+                     <RiSearchLine color="#333333" size={27} />
+                  </button>
+               </form>
+            </div>
+         </div>
+      </div>
+   );
 }
 
-export default Home
+export default Home;
