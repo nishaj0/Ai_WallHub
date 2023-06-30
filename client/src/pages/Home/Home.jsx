5338df259e3dcd131ea281e9ce4adc28639813c5
nishaj0 <njnishaj0@gmail.com>
2023-06-30 11:40:50 +0530

    added method with useState() Hook

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,32 +6,60 @@ import "./home.css";
 
 function Home() {
    const [isSmallScreen, setIsSmallScreen] = useState(false);
+   const [searchData, setSearchData] = useState({ search: "" });
+   console.log(searchData);
 
    useEffect(() => {
       const handleResize = () => {
          setIsSmallScreen(window.innerWidth < 550);
       };
-      handleResize()
+      handleResize();
    }, []);
+
+   const handleSearch = (e) => {
+      setSearchData((prevFormData) => {
+         return {
+            ...prevFormData,
+            [e.target.name]: e.target.value,
+         };
+      });
+   };
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log("searched");
+   };
    return (
-      <div className="wallHub__home section__padding">
-         <div className="wallHub__home-search_container ">
-            <h1>Discover the Future of Wallpapers</h1>
-            <h2> Embrace the Beauty of AI-Generated Wallpapers</h2>
-            <div className="wallHub__home-search_bar">
-               <form action="get">
-                  <input
-                     type="text"
-                     placeholder={
-                        isSmallScreen
-                           ? "Search Wallpaper"
-                           : "Search AI-Generated Wallpapers"
-                     }
-                  />
-                  <button type="submit">
-                     <RiSearchLine color="#333333" size={27} />
-                  </button>
-               </form>
+      <div className="wallHub__home">
+         <div className="wallHub__home-search_div section__padding">
+            <div className="wallHub__home-search_container ">
+               <h1>Discover the Future of Wallpapers</h1>
+               <h2> Embrace the Beauty of AI-Generated Wallpapers</h2>
+               <div className="wallHub__home-search_bar">
+                  <form onSubmit={handleSubmit}>
+                     <input
+                        type="text"
+                        placeholder={
+                           isSmallScreen
+                              ? "Search Wallpaper"
+                              : "Search AI-Generated Wallpapers"
+                        }
+                        onChange={handleSearch}
+                        name="search"
+                        value={searchData.search}
+                     />
+                     <button type="submit">
+                        <RiSearchLine color="#333333" size={27} />
+                     </button>
+                  </form>
+               </div>
+            </div>
+         </div>
+         <div className="wallHub__home-wallpapers section__padding">
+            <div className="wallHub__home-wallpapers_filter-container">
+               <div className="wallHub__home-wallpapers_filter">
+                  <a href=""></a>
+               </div>
             </div>
          </div>
       </div>
