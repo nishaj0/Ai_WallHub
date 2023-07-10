addd84fe298f67780a2afd4d6d7db083df99055c
nishaj0 <njnishaj0@gmail.com>
2023-07-10 20:35:11 +0530

    created basic server

---
+++ server/index.js
@@ -0,0 +1,12 @@
+require("dotenv").config;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+// middleware for json
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
