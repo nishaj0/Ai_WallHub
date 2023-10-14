97c105cb6fedf0c5b2f100e162d4776f46cdf2ca
nishaj0 <njnishaj0@gmail.com>
2023-10-14 20:31:43 +0530

    added json message to verifyJWT

--- server/middlewares/verifyJWT.js
+++ server/middlewares/verifyJWT.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const verifyJWT = (req, res, next) => {
    const authHeader = req.headers.authorization || req.headers.Authorization;
-   if (!authHeader?.startsWith("Bearer")) return res.sendStatus(401); //Unauthorized
+   if (!authHeader?.startsWith("Bearer")) return res.status(401).json({message:'cant find token'}); //Unauthorized
 
    const token = authHeader.split(" ")[1];
    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
