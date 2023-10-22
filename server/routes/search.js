54d5fa35c79666896d6f94cd7159cba9e7adf7bb
nishaj0 <njnishaj0@gmail.com>
2023-10-22 20:55:53 +0530

    fixed searchController not found error

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const searchController = require('../../controllers/searchController');
+const searchController = require('../controllers/searchController');
 
 router.route('/').get(searchController.handleSearch);
 
