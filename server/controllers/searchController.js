a3e3b15cececa2c5a1963e8ba97d999a9ae6d456
nishaj0 <njnishaj0@gmail.com>
2023-11-30 20:52:37 +0530

    Refactor searchController to improve search functionality

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -2,14 +2,14 @@ const Post = require('../model/Post');
 
 const handleSearch = async (req, res) => {
   const keyword = req.body.searchKeyword;
-  // console.log(req.body)
   if (!keyword) return res.status(400).json({ message: 'search keyword required' });
 
-  // ? this will find the titles that contain keyword
-  const postsByTitle = await Post.find({
-    title: {
-      $regex: new RegExp(keyword, 'i'),
-    },
+  const posts = await Post.find({
+    $or: [
+      { title: { $regex: new RegExp(keyword, 'i') } },
+      { prompt: { $regex: new RegExp(keyword, 'i') } },
+      { hashTags: { $elemMatch: { $regex: new RegExp(keyword, 'i') } } }
+    ]
   })
     .select({
       title: 1,
@@ -19,60 +19,7 @@ const handleSearch = async (req, res) => {
     })
     .exec();
 
-  // ? this will find the prompt that contain keyword
-  const postsByPrompt = await Post.find({
-    prompt: {
-      $regex: new RegExp(keyword, 'i'),
-    },
-  })
-    .select({
-      title: 1,
-      prompt: 1,
-      hashTags: 1,
-      publicImgUrl: 1,
-    })
-    .exec();
-  // ? this will find the tags that contain keyword
-  const postsByTag = await Post.find({
-    hashTags: {
-      $elemMatch: {
-        $regex: new RegExp(keyword, 'i'),
-      },
-    },
-  })
-    .select({
-      title: 1,
-      prompt: 1,
-      hashTags: 1,
-      publicImgUrl: 1,
-    })
-    .exec();
-
-  // ? adding everything to one array
-  let finalResult = postsByTitle ? postsByTitle : [];
-
-  // ? if postByPrompt are not empty
-  if (postsByPrompt.length > 0) {
-    for (let i = 0; i < postsByPrompt.length; i++) {
-      // ? if postByPrompt[i] is not in finalArray, then push it into finalArray
-      if (!finalResult.includes(postsByPrompt[i])) {
-        finalResult.push(postsByPrompt[i]);
-      }
-    }
-  }
-
-  // ? if postByTag are not empty
-  if (postsByTag.length > 0) {
-    for (let i = 0; i < postsByTag.length; i++) {
-      // ? if postByTag[i] is not in finalArray, then push it into finalArray
-      if (!finalResult.includes(postsByTag[i])) {
-        finalResult.push(postsByTag[i]);
-      }
-    }
-  }
-
-  // console.log({ postsByTitle });
-  res.status(200).json({ finalResult });
+  res.status(200).json({ finalResult: posts });
 };
 
 module.exports = { handleSearch };
