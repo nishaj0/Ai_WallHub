8e9dcdb23527f8ac79f32749ae6d9e41bdf10c57
nishaj0 <njnishaj0@gmail.com>
2023-12-08 23:58:42 +0530

    created search image album with "react-photo-album"

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 import { useSearchParams, Link } from 'react-router-dom';
 import { RiSearchLine } from 'react-icons/ri';
+import PhotoAlbum from "react-photo-album";
 
 import './search.css';
 import { SearchTag } from '../../components';
@@ -10,7 +11,71 @@ import axios from '../../api/axios';
 
 function Search() {
   // using searchparams
-  const [searchResult, setSearchResult] = useState([]);
+  const [searchResult, setSearchResult] = useState([
+    {
+      title: 'jean at liyue',
+      publicImgUrl:
+        'https://res.cloudinary.com/dvsi6ulf3/image/upload/v1700900205/wallHub/wallpapers/yjkym6ddnptjvawm6cka.png',
+      prompt: '1girl, blue sky, blue and white dress, liyue',
+      hashTags: ['genshin,jean'],
+      width: 480,
+      height: 720,
+    },
+    {
+      title: 'jean with new outfit',
+      publicImgUrl:
+        'https://res.cloudinary.com/dvsi6ulf3/image/upload/v1700900100/wallHub/wallpapers/rnuyvqjo46swwa3g4w7c.png',
+      prompt: '1girl, blue sky, blonde hair',
+      hashTags: ['genshin,jean,genshin-impact,girl'],
+      width: 480,
+      height: 720,
+    },
+    {
+      title: 'Tokyo night',
+      publicImgUrl:
+        'https://res.cloudinary.com/dvsi6ulf3/image/upload/v1701268488/wallHub/wallpapers/xrpldga5l5yptlr7g6ma.jpg',
+      prompt: 'tokyo night view, glowing lights, street',
+      hashTags: ['tokyo,city,glowing'],
+      width: 2016,
+      height: 1120,
+    },
+    {
+      title: 'skye from valorant',
+      publicImgUrl:
+        'https://res.cloudinary.com/dvsi6ulf3/image/upload/v1701267224/wallHub/wallpapers/touh2vgmn3v4dfjlkqxs.png',
+      prompt: '1girl, orange hair, white mountain on background',
+      hashTags: ['valorant,green,skye,mountain'],
+      width: 1024,
+      height: 1808,
+    },
+    {
+      title: 'Night view of Japanese Shrine',
+      publicImgUrl:
+        'https://res.cloudinary.com/dvsi6ulf3/image/upload/v1701267971/wallHub/wallpapers/fchvrzxhiyk3c4q7pqsl.jpg',
+      prompt: 'night view of japanese shrine, sakura tree on background',
+      hashTags: ['japanese,shrine,night,dark'],
+      width: 1176,
+      height: 2072,
+    },
+    {
+      title: 'night view of large mountain',
+      publicImgUrl:
+        'https://res.cloudinary.com/dvsi6ulf3/image/upload/v1701268673/wallHub/wallpapers/ejtjdhvsk87t2lk6gcak.jpg',
+      prompt: 'night view of large mountain, high quality, realistic',
+      hashTags: ['mountain,sky,night'],
+      width: 3072,
+      height: 1024,
+    },
+    {
+      title: 'purple world',
+      publicImgUrl:
+        'https://res.cloudinary.com/dvsi6ulf3/image/upload/v1701268245/wallHub/wallpapers/rjzjgzumiexibosubgiz.jpg',
+      prompt: 'world of electro, purple theme world, large mountains',
+      hashTags: ['purple,electro,mountains'],
+      width: 512,
+      height: 904,
+    },
+  ]);
 
   const [searchParams] = useSearchParams();
   const searchKeyword = searchParams.get('keyword');
@@ -43,6 +108,14 @@ function Search() {
     };
   }, []);
 
+  const images = searchResult.map((img, index) => ({
+    src: img.publicImgUrl,
+    width: img.width,
+    height: img.height,
+  }));
+
+  console.log(images)
+
   return (
     <div className="wallHub__search">
       <div className="wallHub__search-header">
@@ -60,7 +133,10 @@ function Search() {
         <SearchTag searchText={'nature'} content={'Nature'} />
         <SearchTag searchText={'landscape'} content={'Landscape'} />
       </div>
-      <div className="wallHub__search-images">{/* images comes here */}</div>
+      <div className="wallHub__search-images">
+        {/* wallpaper gallery */}
+      <PhotoAlbum layout='masonry' photos={images} />
+      </div>
     </div>
   );
 }
