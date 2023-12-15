5d3debfe01b12cdc4a0417543658c915ce417d7e
nishaj0 <njnishaj0@gmail.com>
2023-12-15 20:10:47 +0530

    Refactor searchController.js, added more properties in response

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -4,22 +4,27 @@ const handleSearch = async (req, res) => {
   const keyword = req.body.searchKeyword;
   if (!keyword) return res.status(400).json({ message: 'search keyword required' });
 
-  const posts = await Post.find({
+  const searchImages = await Post.find({
     $or: [
       { title: { $regex: new RegExp(keyword, 'i') } },
       { prompt: { $regex: new RegExp(keyword, 'i') } },
-      { hashTags: { $elemMatch: { $regex: new RegExp(keyword, 'i') } } }
-    ]
+      { hashTags: { $elemMatch: { $regex: new RegExp(keyword, 'i') } } },
+    ],
   })
     .select({
+      _id:1,
       title: 1,
       prompt: 1,
       hashTags: 1,
       publicImgUrl: 1,
+      likes: 1,
+      createdAt: 1,
+      width: 1,
+      height: 1,
     })
     .exec();
 
-  res.status(200).json({ finalResult: posts });
+  res.status(200).json({ searchImages });
 };
 
 module.exports = { handleSearch };
