cc61fd33dc53beb8d7174334a10439e3158f0480
nishaj0 <njnishaj0@gmail.com>
2023-12-19 20:46:35 +0530

    refactor: conncected wallpaper page with imageController
    wallpaper will get imageDetails when its loaded

--- client/src/pages/Wallpaper/Wallpaper.jsx
+++ client/src/pages/Wallpaper/Wallpaper.jsx
@@ -4,31 +4,47 @@ import './wallpaper.css';
 import { useParams } from 'react-router-dom';
 import { RiUserFill } from 'react-icons/ri';
 import { IoIosHeartEmpty, IoMdHeart } from 'react-icons/io';
+import { saveAs } from 'file-saver';
 
 import useScreenWidth from '../../hooks/useScreenWidth';
+import axios from '../../api/axios';
 
 function Wallpaper() {
-  const [customProfile, setCustomProfile] = useState(false);
   const [isUserLiked, setIsUserLiked] = useState(false);
   const [imageDetails, setImageDetails] = useState({});
+  const [abortController, setAbortController] = useState(null);
   const { id: imageId } = useParams();
   let screenSize = useScreenWidth();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const controller = new AbortController();
+    setAbortController(controller);
+    const fetchImageDetails = async () => {
+      try {
+        const response = await axios.get(`/image/${imageId}`, {
+          signal: controller.signal,
+        });
+        setImageDetails(response.data.imageDetails);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchImageDetails();
+    return () => {
+      if (abortController) controller.abort();
+    };
+  }, []);
+
+  // ? download image when user click download button
+  const downloadImage = () => {
+    saveAs(imageDetails.publicImgUrl, imageDetails.title);
+  };
 
   return (
     <div className="wallHub__wallpaper">
       <div className="wallHub__wallpaper-image_container">
-        <img
-          className="wallHub__wallpaper-image"
-          // style={{
-          //   width: imageDetails.width > imageDetails.height ? '100%' : 'auto',
-          //   height: imageDetails.width > imageDetails.height ? 'auto' : '100%',
-          // }}
-          // src="https://res.cloudinary.com/dvsi6ulf3/image/upload/v1701268673/wallHub/wallpapers/ejtjdhvsk87t2lk6gcak.jpg"
-          src="https://res.cloudinary.com/dvsi6ulf3/image/upload/v1700900100/wallHub/wallpapers/rnuyvqjo46swwa3g4w7c.png"
-          alt="wallpaper image"
-        />
+        <img className="wallHub__wallpaper-image" src={imageDetails.publicImgUrl} alt="wallpaper image" />
       </div>
       <div className="wallHub__wallpaper-info">
         <div className="wallHub__wallpaper-button-container">
@@ -36,20 +52,20 @@ function Wallpaper() {
             {isUserLiked ? <IoMdHeart /> : <IoIosHeartEmpty />}
             <p>203</p>
           </div>
-          <button className="wallHub__wallpaper-button-download">Download</button>
+          <button className="wallHub__wallpaper-button-download" onClick={downloadImage}>
+            Download
+          </button>
         </div>
-        <h2 className="wallHub__wallpaper-title">title</h2>
-        <h4 className="wallHub__wallpaper-prompt">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem odio ipsa praesentium, deleniti nihil
-          perferendis id non, atque necessitatibus animi facilis laborum pariatur aliquid maxime tenetur tempore ratione
-          provident repudiandae.
-        </h4>
+        <h2 className="wallHub__wallpaper-title">{imageDetails.title}</h2>
+        <h4 className="wallHub__wallpaper-prompt">{imageDetails.prompt}</h4>
         <div className="wallHub__wallpaper-about">
           <p>
-            posted by: <a href="#">user031</a>
+            posted by: <a href="#">{imageDetails.userEmail}</a>
           </p>
-          <p>upload date: 1/01/2020</p>
-          <p>size: 300x900</p>
+          <p>upload date:{imageDetails.date}</p>
+          <p>
+            size: {imageDetails.width}x{imageDetails.height}
+          </p>
         </div>
       </div>
     </div>
