51a762db95d924780f0087cbb412f8c829100bec
nishaj0 <njnishaj0@gmail.com>
2024-01-21 13:01:24 +0530

    fix(server): fixed logout controller
    logout controller will clear refresh token that saved in the cookie

--- server/controllers/logoutController.js
+++ server/controllers/logoutController.js
@@ -1,33 +1,31 @@
 const User = require('../model/User');
 
 const handleLogout = async (req, res) => {
-  // * in we are also clearing accessToken
-  const cookies = req.cookies;
-  if (cookies?.jwt) return res.sendStatus(204); // no content
-  const refreshToken = cookies.jwt;
+   const cookies = req.cookies;
 
-  // ? is refreshToken in DB?
-  const foundUser = await User.findOne({ refreshToken }).exec();
-  if (!foundUser) {
-    res.clearCookie('jwt', {
+   if (!cookies?.jwt) return res.sendStatus(204); // no content
+
+   const refreshToken = cookies.jwt;
+   const foundUser = await User.findOne({ refreshToken }).exec();
+   if (!foundUser) {
+      res.clearCookie('jwt', {
+         httpOnly: true,
+         // sameSite:"None",
+         // secure:true
+      });
+      return res.sendStatus(204);
+   }
+
+   // ? delete refreshToken from DB
+   foundUser.refreshToken = '';
+   const result = await foundUser.save();
+
+   res.clearCookie('jwt', {
       httpOnly: true,
       // sameSite:"None",
       // secure:true
-    });
-    return res.sendStatus(204);
-  }
-
-  // ? delete refreshToken from DB
-  foundUser.refreshToken = '';
-  const result = foundUser.save();
-  console.log({ result });
-
-  res.clearCookie('jwt', {
-    httpOnly: true,
-    // sameSite:"None",
-    // secure:true
-  });
-  return res.sendStatus(204);
+   });
+   return res.sendStatus(204);
 };
 
 module.exports = { handleLogout };
