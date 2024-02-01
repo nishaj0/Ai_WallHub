453ba3a0062d578f61434bdebf9fddb6c5808696
nishaj0 <njnishaj0@gmail.com>
2024-02-01 15:31:32 +0530

    refactor(auth): using handleSubmit functionality to login and signup
    removed react router's action funtion to submit form data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,19 @@
 import React from 'react';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from 'react-router-dom';
-import {
-  About,
-  Home,
-  Catagories,
-  Contact,
-  Search,
-  Signup,
-  Login,
-  LoginOtp,
-  ProfileLayout,
-  ProfilePosts,
-  UploadPost,
-  Wallpaper
+   About,
+   Home,
+   Catagories,
+   Contact,
+   Search,
+   Signup,
+   Login,
+   LoginOtp,
+   ProfileLayout,
+   ProfilePosts,
+   UploadPost,
+   Wallpaper,
 } from './pages';
-import { action as signupAction } from './pages/Auth/Signup/Signup.jsx';
-import { action as loginAction } from './pages/Auth/Login/Login.jsx';
 
 import { Layout, PersistLogin } from './containers';
 import useAuth from './hooks/useAuth';
@@ -30,42 +23,39 @@ import useRefreshToken from './hooks/useRefreshToken';
 import './App.css';
 
 function App() {
-  const { auth, setAuth, persist } = useAuth();
-  const axiosPrivate = useAxiosPrivate();
-  const refresh = useRefreshToken();
-  const router = createBrowserRouter(
-    createRoutesFromElements(
-      <>
-        <Route element={<PersistLogin />}>
-          <Route path="*" element={<h2>page not found</h2>} />
-          <Route path="upload-post" element={<UploadPost />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="catagories" element={<Catagories />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="about" element={<About />} />
-            <Route path="search" element={<Search />} />
-            <Route path="image/:id" element={<Wallpaper />} />
+   const { auth, setAuth, persist } = useAuth();
+   const axiosPrivate = useAxiosPrivate();
+   const refresh = useRefreshToken();
+   const router = createBrowserRouter(
+      createRoutesFromElements(
+         <>
+            <Route element={<PersistLogin />}>
+               <Route path="*" element={<h2>page not found</h2>} />
+               <Route path="upload-post" element={<UploadPost />} />
+               <Route path="/" element={<Layout />}>
+                  <Route index element={<Home />} />
+                  <Route path="catagories" element={<Catagories />} />
+                  <Route path="contact" element={<Contact />} />
+                  <Route path="about" element={<About />} />
+                  <Route path="search" element={<Search />} />
+                  <Route path="image/:id" element={<Wallpaper />} />
 
-            {/* protected route */}
-            <Route
-              path="profile"
-              element={<ProfileLayout />}
-            >
-              <Route index element={<ProfilePosts />} />
+                  {/* protected route */}
+                  <Route path="profile" element={<ProfileLayout />}>
+                     <Route index element={<ProfilePosts />} />
+                  </Route>
+               </Route>
             </Route>
-          </Route>
-        </Route>
-        {/* page without header and footer */}
-        {/* Auth pages */}
-        <Route path="signup" action={signupAction} element={<Signup />} />
-        <Route path="login" action={loginAction} element={<Login />} />
-        <Route path="login-otp" element={<LoginOtp />} />
-      </>,
-    ),
-  );
+            {/* page without header and footer */}
+            {/* Auth pages */}
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
+            <Route path="login-otp" element={<LoginOtp />} />
+         </>,
+      ),
+   );
 
-  return <RouterProvider router={router} />;
+   return <RouterProvider router={router} />;
 }
 
 export default App;
--- client/src/pages/Auth/Signup/Signup.jsx
+++ client/src/pages/Auth/Signup/Signup.jsx
@@ -1,231 +1,175 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { RiEyeLine, RiEyeOffLine } from 'react-icons/ri';
 import './signup.css';
-
-import { useState, useEffect } from 'react';
-import {
-  Link,
-  Form,
-  useActionData,
-  useNavigation,
-  useNavigate,
-  useLocation,
-} from 'react-router-dom';
-import { RiEyeLine, RiEyeOffLine, RiErrorWarningFill } from 'react-icons/ri';
-
 import axios from '../../../api/axios';
 import useAuth from '../../../hooks/useAuth';
-
-import { GoogleSignButton } from '../../../components';
 import FormError from '../../../components/FormError/FormError';
+import { GoogleSignButton } from '../../../components';
 
 const SIGNUP_URL = '/register';
 
-export async function action(obj) {
-  const formData = await obj.request.formData();
-  const name = formData.get('name');
-  const username = formData.get('username');
-  const email = formData.get('email');
-  const password = formData.get('password');
-  let errorMessage;
-  let accessToken;
-  try {
-    const res = await axios.post(
-      SIGNUP_URL,
-      JSON.stringify({ name, username, email, password }),
-      {
-        headers: { 'Content-Type': 'application/json' },
-        withCredentials: true,
-      },
-    );
-    accessToken = res?.data?.accessToken;
-  } catch (err) {
-    if (!err?.response) {
-      errorMessage = 'no Server Response';
-    } else if (err.response?.status === 400) {
-      errorMessage = 'missing email or password';
-    } else if (err.response?.status === 401) {
-      errorMessage = 'email or password not match';
-    } else {
-      errorMessage = 'login failed';
-    }
-  }
-  return { errorMessage, userData: { email, accessToken } };
-}
-
 function Signup() {
-  const [isEyeToggle, setIsEyeToggle] = useState(true);
-  const [errorMessage, setErrorMessage] = useState(false);
-  const [passwordData, setPasswordData] = useState({
-    tempPassword: '',
-    password: '',
-  });
-  const actionData = useActionData();
-  const navigation = useNavigation();
-  const navigate = useNavigate();
-  const location = useLocation();
+   const [isEyeToggle, setIsEyeToggle] = useState(true);
+   const [errorMessage, setErrorMessage] = useState(false);
+   const [formData, setFormData] = useState({ name: '', username: '', email: '' });
+   const [isSubmitting, setIsSubmitting] = useState(false);
+   const [passwordData, setPasswordData] = useState({
+      tempPassword: '',
+      password: '',
+   });
 
-  const { auth, setAuth, persist, setPersist } = useAuth();
-  const from = location.state?.from?.pathname || '/';
+   const navigate = useNavigate();
+   const location = useLocation();
 
-  // ? useEffect to set the error message and auth state when action is called
-  useEffect(() => {
-    setErrorMessage(actionData?.errorMessage);
-    setAuth((prev) => {
-      if (actionData?.userData) {
-        return actionData.userData;
-      } else {
-        return prev;
+   const { setAuth, persist, setPersist } = useAuth();
+   const from = location.state?.from?.pathname || '/';
+
+   useEffect(() => {
+      setErrorMessage(passwordData.password !== passwordData.tempPassword ? 'Password not matched' : false);
+   }, [passwordData]);
+
+   useEffect(() => {
+      localStorage.setItem('persist', persist);
+   }, [persist]);
+
+   const togglePersist = () => {
+      setPersist((prev) => !prev);
+   };
+
+   const handlePassword = (e) => {
+      setPasswordData((prevPassword) => ({
+         ...prevPassword,
+         [e.target.name]: e.target.value,
+      }));
+   };
+
+   const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData((prevFormData) => ({
+         ...prevFormData,
+         [name]: value,
+      }));
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      setIsSubmitting(true);
+      try {
+         const res = await axios.post(SIGNUP_URL, JSON.stringify({ ...formData, password: passwordData.password }), {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true,
+         });
+         setAuth({ email: res.data.email, accessToken: res.data.accessToken });
+         navigate(from, { replace: true });
+      } catch (err) {
+         if (!err?.response) {
+            setErrorMessage('no Server Response');
+         } else if (err.response?.status === 400) {
+            setErrorMessage('missing email or password');
+         } else if (err.response?.status === 401) {
+            setErrorMessage('email or password not match');
+         } else {
+            setErrorMessage('login failed');
+         }
+      } finally {
+         setIsSubmitting(false);
       }
-    });
-  }, [actionData, navigation.state]);
+   };
 
-  // ? function to handle the password
-  const handlePassword = (e) => {
-    setPasswordData((prevFormData) => {
-      return {
-        ...prevFormData,
-        [e.target.name]: e.target.value,
-      };
-    });
-  };
-
-  // ? useEffect to set the error message when the password is not matched
-  useEffect(() => {
-    if (passwordData.password !== passwordData.tempPassword) {
-      setErrorMessage('Password not matched');
-    } else {
-      setErrorMessage(false);
-    }
-  }, [passwordData]);
-
-  // ? useEffect to set the persist state in the local storage
-  useEffect(() => {
-    localStorage.setItem('persist', persist);
-  }, [persist]);
-
-  // ? toggle the persist state
-  const togglePersist = () => {
-    setPersist((prev) => !prev);
-  };
-
-  // ? useEffect to redirect to the from path when the user is signed up
-  useEffect(() => {
-    if (actionData?.userData?.accessToken) navigate(from, { replace: true });
-  }, [actionData]);
-
-  return (
-    <div className="wallHub__signup">
-      <div className="wallHub__signup-head">
-        <h2>Create your account</h2>
+   return (
+      <div className="wallHub__signup">
+         <div className="wallHub__signup-head">
+            <h2>Create your account</h2>
+         </div>
+         <div className="wallHub__signup-container">
+            <form onSubmit={handleSubmit}>
+               <div className="wallHub__signup-label_container">
+                  <label htmlFor="wallHub__signup-name">Full Name</label>
+               </div>
+               <input
+                  id="wallHub__signup-name"
+                  className="wallHub__signup-name"
+                  name="name"
+                  placeholder="John Doe"
+                  type="name"
+                  onChange={handleChange}
+                  required
+               />
+               <div className="wallHub__signup-label_container">
+                  <label htmlFor="wallHub__signup-name">User Name</label>
+               </div>
+               <input
+                  id="wallHub__signup-username"
+                  className="wallHub__signup-username"
+                  name="username"
+                  placeholder="john.doe"
+                  type="username"
+                  onChange={handleChange}
+                  required
+               />
+               <div className="wallHub__signup-label_container">
+                  <label htmlFor="wallHub__signup-email">Email address</label>
+               </div>
+               <input
+                  id="wallHub__signup-email"
+                  className="wallHub__signup-email"
+                  name="email"
+                  placeholder="john.doe@example.com"
+                  type="email"
+                  onChange={handleChange}
+                  required
+               />
+               <div className="wallHub__signup-label_container">
+                  <label htmlFor="wallHub__signup-password">Password</label>
+               </div>
+               <div className="wallHub__signup-password_container">
+                  <input
+                     onChange={handlePassword}
+                     name="tempPassword"
+                     className="wallHub__signup-password"
+                     placeholder="Enter password"
+                     type={isEyeToggle ? 'password' : 'text'}
+                     pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
+                     title="Password must be at least 8 characters long and contain at least one letter and one number"
+                     required
+                  />
+                  <span className="wallHub__signup-password_eye-span" onClick={() => setIsEyeToggle((prev) => !prev)}>
+                     {isEyeToggle ? <RiEyeOffLine color="#333" size={23} /> : <RiEyeLine color="#333" size={23} />}
+                  </span>
+               </div>
+               <div className="wallHub__signup-password_container">
+                  <input
+                     onChange={handlePassword}
+                     className="wallHub__signup-password"
+                     name="password"
+                     placeholder="Confirm password"
+                     type={isEyeToggle ? 'password' : 'text'}
+                     required
+                  />
+                  <span className="wallHub__signup-password_eye-span" onClick={() => setIsEyeToggle((prev) => !prev)}>
+                     {isEyeToggle ? <RiEyeOffLine color="#333" size={23} /> : <RiEyeLine color="#333" size={23} />}
+                  </span>
+               </div>
+               {errorMessage && <FormError errorMessage={errorMessage} />}
+               <button disabled={isSubmitting}>{isSubmitting ? 'Signing...' : 'Sign up'}</button>
+               <div className="wallHub__login-checkbox">
+                  <input type="checkbox" id="persist" onChange={togglePersist} checked={persist} />
+                  <label htmlFor="persist">Trust this device</label>
+               </div>
+               <p className="wallHub__signup-agreement_p">
+                  By registering, you agree to aiWallHub's <Link to={'/terms-condition'}>Terms and Condition</Link> and{' '}
+                  <Link to={'/privacy-policy'}>Privacy and Policy</Link>
+               </p>
+            </form>
+            <hr className="wallHub__signup-hr" />
+            <GoogleSignButton />
+            <Link className="wallHub__signup-link_login" to={'/login'}>
+               Already have an account?
+            </Link>
+         </div>
       </div>
-      <div className="wallHub__signup-container">
-        <Form method="post">
-          <div className="wallHub__signup-label_container">
-            <label htmlFor="wallHub__signup-name">Full Name</label>
-          </div>
-          <input
-            id="wallHub__signup-name"
-            className="wallHub__signup-name"
-            name="name"
-            placeholder="John Doe"
-            type="name"
-            required
-          />
-          <div className="wallHub__signup-label_container">
-            <label htmlFor="wallHub__signup-name">User Name</label>
-          </div>
-          <input
-            id="wallHub__signup-username"
-            className="wallHub__signup-username"
-            name="username"
-            placeholder="john.doe"
-            type="username"
-            required
-          />
-          <div className="wallHub__signup-label_container">
-            <label htmlFor="wallHub__signup-email">Email address</label>
-          </div>
-          <input
-            id="wallHub__signup-email"
-            className="wallHub__signup-email"
-            name="email"
-            placeholder="john.doe@example.com"
-            type="email"
-            required
-          />
-          <div className="wallHub__signup-label_container">
-            <label htmlFor="wallHub__signup-password">Password</label>
-          </div>
-          <div className="wallHub__signup-password_container">
-            <input
-              onChange={handlePassword}
-              name="tempPassword"
-              className="wallHub__signup-password"
-              placeholder="Enter password"
-              type={isEyeToggle ? 'password' : 'text'}
-              pattern="^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$"
-              title="Password must be at least 8 characters long and contain at least one letter and one number"
-              required
-            />
-            <span
-              className="wallHub__signup-password_eye-span"
-              onClick={() => setIsEyeToggle((prev) => !prev)}
-            >
-              {isEyeToggle ? (
-                <RiEyeOffLine color="#333" size={23} />
-              ) : (
-                <RiEyeLine color="#333" size={23} />
-              )}
-            </span>
-          </div>
-          <div className="wallHub__signup-password_container">
-            <input
-              onChange={handlePassword}
-              className="wallHub__signup-password"
-              name="password"
-              placeholder="Confirm password"
-              type={isEyeToggle ? 'password' : 'text'}
-              required
-            />
-            <span
-              className="wallHub__signup-password_eye-span"
-              onClick={() => setIsEyeToggle((prev) => !prev)}
-            >
-              {isEyeToggle ? (
-                <RiEyeOffLine color="#333" size={23} />
-              ) : (
-                <RiEyeLine color="#333" size={23} />
-              )}
-            </span>
-          </div>
-          {errorMessage && <FormError errorMessage={errorMessage} />}
-          <button disabled={navigation.state === 'submitting'}>
-            {navigation.state === 'submitting' ? 'Signing...' : 'Sign up'}
-          </button>
-          <div className="wallHub__login-checkbox">
-            <input
-              type="checkbox"
-              id="persist"
-              onChange={togglePersist}
-              checked={persist}
-            />
-            {/* <span className="wallHub__login-checkbox_custom-icon"></span> */}
-            <label htmlFor="persist">Trust this device</label>
-          </div>
-          <p className="wallHub__signup-agreement_p">
-            By registering, you agree to aiWallHub's{' '}
-            <Link to={'/terms-condition'}>Terms and Condition</Link> and{' '}
-            <Link to={'/privacy-policy'}>Privacy and Policy</Link>
-          </p>
-        </Form>
-        <hr className="wallHub__signup-hr" />
-        <GoogleSignButton />
-        <Link className="wallHub__signup-link_login" to={'/login'}>
-          Already have an account?
-        </Link>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default Signup;
