7e1f4a60bedd0495e1828db78bf84cf02e8c7b29
nishaj0 <njnishaj0@gmail.com>
2024-02-01 13:58:24 +0530

    feat(client): add redux packages, created store for redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
 import App from './App.jsx';
 import './index.css';
 import { AuthProvider } from './context/AuthProvider.jsx';
+import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </React.StrictMode>,
+   <React.StrictMode>
+      <AuthProvider>
+         <Provider store={store}>
+            <App />
+         </Provider>
+      </AuthProvider>
+   </React.StrictMode>,
 );
