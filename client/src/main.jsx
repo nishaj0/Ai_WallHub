cad26ed8a944b9f77c1ff93539e2095835ca1d04
nishaj0 <njnishaj0@gmail.com>
2024-02-05 14:10:05 +0530

    refactor(client): remove AuthProvider context and useAuth hook
    now auth context is not using, using redux state instead of context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,12 @@ import { Provider } from 'react-redux';
 
 import App from './App.jsx';
 import './index.css';
-import { AuthProvider } from './context/AuthProvider.jsx';
 import { store } from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
    <React.StrictMode>
-      <AuthProvider>
          <Provider store={store}>
             <App />
          </Provider>
-      </AuthProvider>
    </React.StrictMode>,
 );
