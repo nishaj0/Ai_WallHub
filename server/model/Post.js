616aff3bd85913d4cf6bde6f98232b366723c318
nishaj0 <njnishaj0@gmail.com>
2024-02-06 02:17:45 +0530

    refactor(server): refactor post and user models for better readability
    upload image and user auth functionality will not work

--- server/model/Post.js
+++ server/model/Post.js
@@ -1,48 +1,48 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const postSchema = new Schema({
-   title: {
-      type: String,
-      required: true,
+const postSchema = new Schema(
+   {
+      title: {
+         type: String,
+         required: true,
+      },
+      cloudinaryAssetId: {
+         type: String,
+         required: true,
+      },
+      cloudinaryPublicId: {
+         type: String,
+         required: true,
+      },
+      url: {
+         type: String,
+         required: true,
+      },
+      prompt: {
+         type: String,
+      },
+      hashTags: {
+         type: [String],
+      },
+      width: {
+         type: Number,
+         required: true,
+      },
+      height: {
+         type: Number,
+         required: true,
+      },
+      // this will store the id of the user who uploaded the image
+      userRef: {
+         type: String,
+         required: true,
+      },
+      likedBy: {
+         type: [String],
+      },
    },
-   cloudinaryAsset_id: {
-      type: String,
-      required: true,
-   },
-   cloudinaryPublic_id: {
-      type: String,
-      required: true,
-   },
-   // this will store the public url of the image stored in cloudinary
-   publicImgUrl: {
-      type: String,
-      required: true,
-   },
-   prompt: {
-      type: String,
-   },
-   hashTags: {
-      type: [String],
-   },
-   // this will store the username of the user who posted the post
-   userEmail: {
-      type: String,
-      required: true,
-   },
-   width: {
-      type: Number,
-      required: true,
-   },
-   height: {
-      type: Number,
-      required: true,
-   },
-   // this will store the date of the post's creation
-   date: {
-      type: Date,
-      default: Date.now,
-   },
-});
+   { timestamps: true },
+);
 
 module.exports = mongoose.model('Post', postSchema);
--- server/model/User.js
+++ server/model/User.js
@@ -1,47 +1,45 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-   name: {
-      type: String,
-      required: true,
+const userSchema = new Schema(
+   {
+      fullName: {
+         type: String,
+         required: true,
+      },
+      username: {
+         type: String,
+         required: true,
+         unique: true,
+      },
+      role: {
+         type: String,
+         required: true,
+         default: 'user',
+         enum: ['user', 'admin', 'moderator'],
+      },
+      email: {
+         type: String,
+         required: true,
+         unique: true,
+      },
+      password: {
+         type: String,
+         required: true,
+      },
+      refreshToken: {
+         type: String,
+      },
+      posts: {
+         type: [String],
+      },
+      likedPosts: {
+         type: [String],
+      },
    },
-   username: {
-      type: String,
-      required: true,
-      unique: true,
+   {
+      timestamps: true,
    },
-   roles: {
-      type: String,
-      required: true,
-      default: 'user',
-      enum: ['user', 'admin', 'moderator'],
-   },
-   email: {
-      type: String,
-      required: true,
-      unique: true,
-   },
-   password: {
-      type: String,
-      required: true,
-   },
-   refreshToken: String,
-   //this will store the date of the user's account creation
-   date: {
-      type: Date,
-      default: Date.now,
-   },
-   // this will store the post id of the posts that the user has posted
-   posts: {
-      type: [String],
-   },
-   likedPosts: {
-      type: [String],
-   },
-}); // ? add { collection: 'collectionName' } as second parameter of Schema
-// ? if you want to specify a collection name
-// ? (by default mongoose will create a collection based on the model name)
-// ? e.g. mongoose.model("User", userSchema, { collection: "users" });
+);
 
 module.exports = mongoose.model('User', userSchema);
