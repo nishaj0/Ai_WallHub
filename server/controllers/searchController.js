7aedde65fd66acd45200b1838c6111f569691a72
nishaj0 <njnishaj0@gmail.com>
2024-02-06 17:53:04 +0530

    refactor(server): refactor searchController and search router
    use middlware error handling, changed searchByKeyword request from POST to GET
    
    client side searching will not work properly

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,38 +1,56 @@
 const Post = require('../model/Post');
+const returnError = require('../util/returnError');
 
-const searchByKeyword = async (req, res) => {
-   const keyword = req.body.searchKeyword;
-   if (!keyword) return res.status(400).json({ message: 'search keyword required' });
+const searchByKeyword = async (req, res, next) => {
+   const { keyword } = req.query;
+   if (!keyword) return next(returnError(400, 'search keyword is required'));
 
-   const searchImages = await Post.find({
-      $or: [
-         { title: { $regex: new RegExp(keyword, 'i') } },
-         { prompt: { $regex: new RegExp(keyword, 'i') } },
-         { hashTags: { $elemMatch: { $regex: new RegExp(keyword, 'i') } } },
-      ],
-   })
-      .select({
-         _id: 1,
+   try {
+      const searchImages = await Post.find({
+         $or: [
+            { title: { $regex: new RegExp(keyword, 'i') } },
+            { prompt: { $regex: new RegExp(keyword, 'i') } },
+            { hashTags: { $elemMatch: { $regex: new RegExp(keyword, 'i') } } },
+         ],
+      }).select({
+         id: 1,
          title: 1,
          prompt: 1,
          hashTags: 1,
-         publicImgUrl: 1,
+         url: 1,
          likes: 1,
          createdAt: 1,
          width: 1,
          height: 1,
-      })
-      .exec();
+      });
 
-   res.status(200).json({ searchImages });
-};
-
-const get20Images = async (req, res) => {
-   try {
-      const images = await Post.find().sort({ date: -1 }).limit(20).exec();
-      res.status(200).json({ images });
+      res.status(200).json(searchImages);
    } catch (error) {
       console.log(error);
+      next(error);
+   }
+};
+
+const get20Images = async (req, res, next) => {
+   try {
+      const images = await Post.find()
+         .select({
+            id: 1,
+            title: 1,
+            prompt: 1,
+            hashTags: 1,
+            url: 1,
+            likes: 1,
+            createdAt: 1,
+            width: 1,
+            height: 1,
+         })
+         .sort({ createdAt: -1 })
+         .limit(20);
+      res.status(200).json(images);
+   } catch (error) {
+      console.log(error);
+      next(error);
    }
 };
 
--- server/routes/search.js
+++ server/routes/search.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const searchController = require('../controllers/searchController');
 
-router.route('/').post(searchController.searchByKeyword);
+router.route('/').get(searchController.searchByKeyword);
 router.route('/home').get(searchController.get20Images);
 
 module.exports = router;
