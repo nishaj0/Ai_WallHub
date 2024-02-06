18fa365cbf4bacade2e08b35f05035fac34c42e4
nishaj0 <njnishaj0@gmail.com>
2024-02-06 17:45:41 +0530

    refactor(server): Refactor verifyJWT middleware

--- server/middlewares/verifyJWT.js
+++ server/middlewares/verifyJWT.js
@@ -1,15 +1,17 @@
 const jwt = require('jsonwebtoken');
+const returnError = require('../util/returnError');
 
 const verifyJWT = (req, res, next) => {
    const authHeader = req.headers.authorization || req.headers.Authorization;
-   if (!authHeader?.startsWith('Bearer')) return res.status(401).json({ message: 'cant find token' }); //Unauthorized
+   if (!authHeader?.startsWith('Bearer')) return res.status(401).json({ message: 'cant find access token' });
 
    const token = authHeader.split(' ')[1];
    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) return res.sendStatus(403); //forbidden
-      req.email = decoded.userInfo.email;
+      if (err) return next(returnError(401, 'invalid token'));
+
+      req.userId = decoded.userInfo.userId;
       req.username = decoded.userInfo.username;
-      req.roles = decoded.userInfo.roles;
+      req.role = decoded.userInfo.role;
       next();
    });
 };
