8f372d83cdeda890a2e5aaad02707a7bfdbae75e
nishaj0 <njnishaj0@gmail.com>
2024-05-11 14:07:59 +0530

    feat(client): add delete Wallpaper option
    only post owner can see a options button in `Wallpaper` page with delete option

--- client/src/pages/Wallpaper/Wallpaper.jsx
+++ client/src/pages/Wallpaper/Wallpaper.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
 import { saveAs } from 'file-saver';
 import { useSelector } from 'react-redux';
+import { SlOptionsVertical } from 'react-icons/sl';
 import './wallpaper.css';
 import axios from '../../api/axios';
-import { LoadingSvg, BlueButton } from '../../components';
+import { LoadingSvg, BlueButton, ToggleMenu, ConfirmDialogBox } from '../../components';
 import PostLike from './PostLike/PostLike';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 
 function Wallpaper() {
    const [isUserLiked, setIsUserLiked] = useState(false);
@@ -13,16 +15,24 @@ function Wallpaper() {
    const [abortController, setAbortController] = useState(null);
    const [isLoading, setIsLoading] = useState(true);
    const [likeTrigger, setLikeTrigger] = useState(false);
+   const [toggleMenu, setToggleMenu] = useState(false);
+   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
 
    const { id: imageId } = useParams();
+   const location = useLocation();
+   const navigate = useNavigate();
+   const axiosPrivate = useAxiosPrivate();
    const currentUserId = useSelector((state) => state.user.userId);
+
+   const IMAGE_URL = `/api/post/${imageId}`;
+   const from = location.state?.from?.pathname || '/';
 
    useEffect(() => {
       const controller = new AbortController();
       setAbortController(controller);
       const fetchImageDetails = async () => {
          try {
-            const response = await axios.get(`/api/post/${imageId}`, {
+            const response = await axios.get(IMAGE_URL, {
                signal: controller.signal,
             });
 
@@ -46,6 +56,23 @@ function Wallpaper() {
          if (abortController) controller.abort();
       };
    }, [likeTrigger]);
+
+   const handleToggleButton = () => {
+      setToggleMenu(!toggleMenu);
+
+      // ? close confirm dialog box if it is open
+      if (isConfirmOpen) setIsConfirmOpen(false);
+   };
+
+   const handlePostDelete = async () => {
+      try {
+         setIsConfirmOpen(false);
+         const res = await axiosPrivate.delete(IMAGE_URL);
+         navigate(from);
+      } catch (err) {
+         console.error(err);
+      }
+   };
 
    // ? download image when user click download button
    const downloadImage = () => {
@@ -81,7 +108,44 @@ function Wallpaper() {
                         currentUserId={currentUserId}
                         setLikeTrigger={setLikeTrigger}
                      />
-                     <BlueButton onClick={downloadImage}>Download</BlueButton>
+                     <div className="wallHub__wallpaper-download-edit-buttons">
+                        <BlueButton onClick={downloadImage}>Download</BlueButton>
+                        {imageDetails.userRef === currentUserId && (
+                           <>
+                              <button onClick={handleToggleButton} className="wallHub__wallpaper-menu-button">
+                                 <SlOptionsVertical size={25} color="333333" />
+                              </button>
+                              {toggleMenu && (
+                                 <ToggleMenu className="wallHub__wallpaper-menu">
+                                    <Link
+                                       onClick={() => setIsConfirmOpen(true)}
+                                       className="wallHub__wallpaper-menu-delete-button"
+                                    >
+                                       delete post
+                                    </Link>
+                                    <ConfirmDialogBox
+                                       isOpen={isConfirmOpen}
+                                       question={'Are you sure you want to delete this post'}
+                                       description={'This action cannot be undone.'}
+                                    >
+                                       <button
+                                          onClick={handlePostDelete}
+                                          className="wallHub__wallpaper-confirmBox-button-confirm"
+                                       >
+                                          Confirm
+                                       </button>
+                                       <button
+                                          onClick={() => setIsConfirmOpen(false)}
+                                          className="wallHub__wallpaper-confirmBox-button-cancel"
+                                       >
+                                          Cancel
+                                       </button>
+                                    </ConfirmDialogBox>
+                                 </ToggleMenu>
+                              )}
+                           </>
+                        )}
+                     </div>
                   </div>
                   <h2 className="wallHub__wallpaper-title">{imageDetails.title}</h2>
                   <h4 className="wallHub__wallpaper-prompt">{imageDetails.prompt}</h4>
