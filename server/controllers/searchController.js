ff30c07137f3763dcbc905234aa094f7c5acba38
nishaj0 <njnishaj0@gmail.com>
2024-05-14 09:02:11 +0530

    refactor(search): Remove getRecentPost function from searchController
    this cause an error in `client` side, `home` page will not display any image because of the endpoint removal

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -31,30 +31,4 @@ const searchPostsByKeyword = async (req, res, next) => {
    }
 };
 
-const getRecentPost = async (req, res, next) => {
-   // ? if there is no limit query, it will return all posts
-   const limit = req.query.limit || {};
-
-   try {
-      const posts = await Post.find()
-         .select({
-            id: 1,
-            title: 1,
-            prompt: 1,
-            hashTags: 1,
-            url: 1,
-            likes: 1,
-            createdAt: 1,
-            width: 1,
-            height: 1,
-         })
-         .sort({ createdAt: -1 })
-         .limit(limit);
-      res.status(200).json(posts);
-   } catch (error) {
-      console.log(error);
-      next(error);
-   }
-};
-
-module.exports = { searchPostsByKeyword, getRecentPost };
+module.exports = { searchPostsByKeyword };
--- server/routes/search.js
+++ server/routes/search.js
@@ -4,6 +4,5 @@ const searchController = require('../controllers/searchController');
 const router = express.Router();
 
 router.get('/', searchController.searchPostsByKeyword);
-router.get('/recent', searchController.getRecentPost);
 
 module.exports = router;
