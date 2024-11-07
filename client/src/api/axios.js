ff604db482410f7feaec3c904df74a75d99205fd
nishaj0 <njnishaj0@gmail.com>
2024-11-07 23:29:16 +0530

    build: update axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-const BASE_URL = 'http://localhost:5000/';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export default axios.create({
-   baseURL: import.meta.env.VITE_BASE_URL,
+   baseURL: BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
