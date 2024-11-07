52ca33e98cd747849463b0b4d769946e9041c53c
nishaj0 <njnishaj0@gmail.com>
2024-11-07 23:02:11 +0530

    build: add env

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:5000/';
 
 export default axios.create({
-   baseURL: BASE_URL,
+   baseURL: import.meta.env.VITE_BASE_URL,
 });
 
 export const axiosPrivate = axios.create({
